04965e5b12ea1be04fdf1fcc8cca3c04dbf60e2b
Rajat <rajatprajapati09@gmail.com>
2023-03-31 23:28:26 +0530

    Calculating EmpWage for Month

--- Program.cs
+++ Program.cs
@@ -4,35 +4,36 @@ namespace EmployeeWage
 {
     internal class Program
     {
-        const int empWagePerHour = 20;
+        const int workingDays = 20;
+        const int empPerHourWage = 20;
         static void Main(string[] args)
         {
             int empPerDayHour = 0;
-
+            int totalEmployeeWage = 0;
+           
             Console.WriteLine("Welcome To EmployeeWage Computation Program\n");
             Random rnd = new Random();
-            int checke=rnd.Next(0,3);
-            
-            switch(checke)
+            for(int i = 0; i < workingDays; i++) 
             {
-                case 1:
-                       Console.WriteLine("Emplyee is Present For Full Day");
-                       empPerDayHour = 8;
-                       break;
-                case 2:
-                       Console.WriteLine("Emplyee is Present For Half Day");
-                       empPerDayHour = 4;
-                       break; 
-                default:
-                        Console.WriteLine("Emplyee is Absent");
-                        empPerDayHour = 0;
-                    break;
+                int checke = rnd.Next(0, 3);
+                switch (checke)
+                {
+                    case 1:
+                            empPerDayHour = 8;
+                        break;
+                    case 2:
+                            empPerDayHour = 4;
+                        break;
+                    default:
+                            empPerDayHour = 0;
+                        break;
+                }
+                int empDailyWage = empPerDayHour * empPerHourWage;
+                totalEmployeeWage+=empDailyWage;
             }
-            int empDailyWage = empPerDayHour * empWagePerHour;
-            Console.WriteLine($"Employee Daily Wage Is :{empDailyWage}$");
-
-           Console.ReadLine();
-
+          
+            Console.WriteLine($"Employee Monthly Wage Is :{totalEmployeeWage}$");
+            Console.ReadLine();
         }
     }
 }
