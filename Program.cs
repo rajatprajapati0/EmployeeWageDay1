b550f9bb579154773faa9543946b730c03d1411f
Rajat <rajatprajapati09@gmail.com>
2023-03-31 23:45:09 +0530

    calculating wage till 100 hours or till 20 days

--- Program.cs
+++ Program.cs
@@ -4,17 +4,20 @@ namespace EmployeeWage
 {
     internal class Program
     {
-        const int workingDays = 20;
+        const int maxWorkingDays = 20;
+        const int maxWorkoingHours = 100;
         const int empPerHourWage = 20;
         static void Main(string[] args)
         {
             int empPerDayHour = 0;
             int totalEmployeeWage = 0;
-           
+            int workingDays = 0;
+            int workoingHours = 0;
             Console.WriteLine("Welcome To EmployeeWage Computation Program\n");
             Random rnd = new Random();
-            for(int i = 0; i < workingDays; i++) 
+            while(workingDays < maxWorkingDays && workoingHours < maxWorkoingHours) 
             {
+                workingDays++;
                 int checke = rnd.Next(0, 3);
                 switch (checke)
                 {
@@ -28,11 +31,15 @@ namespace EmployeeWage
                             empPerDayHour = 0;
                         break;
                 }
+                workoingHours+=empPerDayHour;
+
                 int empDailyWage = empPerDayHour * empPerHourWage;
                 totalEmployeeWage+=empDailyWage;
             }
           
-            Console.WriteLine($"Employee Monthly Wage Is :{totalEmployeeWage}$");
+            Console.WriteLine($"Employee Monthly Wage Is :{totalEmployeeWage}$\n" +
+                              $"working Days in month :{workingDays}\n" +
+                              $"working hours in month:{workoingHours}\n");
             Console.ReadLine();
         }
     }
