221fc75400744809dea5021c0645e9927d6b672a
Rajat <rajatprajapati09@gmail.com>
2023-03-31 22:21:08 +0530

    calculating Daily EmpWage

--- Program.cs
+++ Program.cs
@@ -5,21 +5,31 @@ namespace EmployeeWage
     internal class Program
     {
         const int present = 1;
+        const int empWagePerHour = 20;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To EmployeeWage Computation Program");
-           Random rnd = new Random();
-           int checke=rnd.Next(0,2);
+            int empPerDayHour = 0;
+
+            Console.WriteLine("Welcome To EmployeeWage Computation Program\n");
+            Random rnd = new Random();
+            int checke=rnd.Next(0,2);
             
             if (checke==present)
             {
                 Console.WriteLine("Emplyee is Present");
+                empPerDayHour = 8;
             }
             else
             {
                 Console.WriteLine("Emplyee is Absent");
-
+                empPerDayHour = 0;
+                
             }
+
+            int empDailyWage = empPerDayHour * empWagePerHour;
+            Console.WriteLine($"Employee Daily Wage Is :{empDailyWage}$");
+
+            Console.ReadLine();
 
         }
     }
